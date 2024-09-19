ac8eaa4da9286991b5024945b7a9150002a6cd40
AndrewDul <dulandrew.dev@gmail.com>
2024-09-19 20:57:38 +0100

    v0.0

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { singup, login, logout } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.get("/singup", singup);
+
+router.get("/login", login);
+
+router.get("/logout", logout);
+export default router;
