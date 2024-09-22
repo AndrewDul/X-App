362243cece6337792ab337aa0d0d0cef00971bf5
AndrewDul <dulandrew.dev@gmail.com>
2024-09-22 12:24:59 +0100

    all-auth-add-started-code-profile-user-route-followUnfollow-just-started-controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,11 +5,39 @@ export const getUserProfile = async (req, res) => {
   try {
     const user = await User.findOne({ username }).select("-password");
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ message: "User not found" });
     }
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ error: error.message });
     console.log("Error in getUserProfile :", error.message);
+  }
+};
+
+export const followUnfollowUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+
+    if (id === req.user._id) {
+      return res
+        .status(400)
+        .json({ error: "You can't follow / unfollow yourself" });
+    }
+    if (!userToModify || currentUser) {
+      return res.status(400).json({ error: "User not found" });
+    }
+
+    const isFollowing = currentUser.following.includes(id);
+
+    if (isFollowing) {
+      //Unfollow the user
+    } else {
+      //follow the user
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in followUnfollowUser :", error.message);
   }
 };
