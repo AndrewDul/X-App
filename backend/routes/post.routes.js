68da3ec8f125026e79421c725da9a26c8df19ba0
AndrewDul <dulandrew.dev@gmail.com>
2024-09-25 11:38:05 +0100

    post-routes-and-controller-have-been-added

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -4,12 +4,19 @@ import {
   deletePost,
   commentOnPost,
   likeUnlikePost,
+  getAllPosts,
+  getLikedPosts,
+  getFollowingPosts,
+  getUserPosts,
 } from "../controllers/post.controller.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-//router.get("/all", protectRoute, getAllPosts);
+router.get("/all", protectRoute, getAllPosts);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/likes/:id", protectRoute, getLikedPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
 router.post("/create", protectRoute, createPost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
