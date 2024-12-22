c3fbfa842c41a57f91803bf654a672d455f3e94a
AndrewDul <dulandrew.dev@gmail.com>
2024-12-22 20:39:28 +0000

    Profile_Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import SignUpPage from "./pages/auth/signup/SignUpPage";
 import RightPanel from "./components/common/RightPanel";
 import Sidebar from "./components/common/Sidebar";
 import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="notifications" element={<NotificationPage />} />
+        <Route path="profile/:username" element={<ProfilePage />} />
       </Routes>
       <RightPanel />
     </div>
